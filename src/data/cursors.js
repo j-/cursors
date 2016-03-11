ed674f65b4c58f622c291b994b7a76d3c334c04c
Jamie Hoeks <j@skeoh.com>
2016-03-11 19:15:20 +1100

    Move JS into src/

---
+++ src/data/cursors.js
@@ -0,0 +1,394 @@
+export default [
+	{
+		"type": "-moz-grab",
+		"matches": [
+			"-moz-grab",
+			"grab"
+		],
+		"category": "grab",
+		"vendor": true,
+	},
+	{
+		"type": "-moz-grabbing",
+		"matches": [
+			"-moz-grabbing",
+			"grabbing"
+		],
+		"category": "grab",
+		"vendor": true,
+	},
+	{
+		"type": "-moz-zoom-in",
+		"matches": [
+			"-moz-zoom-in",
+			"zoom-in"
+		],
+		"category": "zoom",
+		"vendor": true,
+	},
+	{
+		"type": "-moz-zoom-out",
+		"matches": [
+			"-moz-zoom-out",
+			"zoom-out"
+		],
+		"category": "zoom",
+		"vendor": true,
+	},
+	{
+		"type": "-webkit-grab",
+		"matches": [
+			"-webkit-grab",
+			"grab"
+		],
+		"category": "grab",
+		"vendor": true,
+	},
+	{
+		"type": "-webkit-grabbing",
+		"matches": [
+			"-webkit-grabbing",
+			"grabbing"
+		],
+		"category": "grab",
+		"vendor": true,
+	},
+	{
+		"type": "-webkit-zoom-in",
+		"matches": [
+			"-webkit-zoom-in",
+			"zoom-in"
+		],
+		"category": "zoom",
+		"vendor": true,
+	},
+	{
+		"type": "-webkit-zoom-out",
+		"matches": [
+			"-webkit-zoom-out",
+			"zoom-out"
+		],
+		"category": "zoom",
+		"vendor": true,
+	},
+	{
+		"type": "alias",
+		"matches": [
+			"alias"
+		],
+		"category": "drag-and-drop",
+		"vendor": false,
+	},
+	{
+		"type": "all-scroll",
+		"matches": [
+			"all-scroll"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "auto",
+		"matches": [
+			"auto"
+		],
+		"category": "general",
+		"vendor": false,
+	},
+	{
+		"type": "cell",
+		"matches": [
+			"cell"
+		],
+		"category": "selection",
+		"vendor": false,
+	},
+	{
+		"type": "col-resize",
+		"matches": [
+			"col-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "context-menu",
+		"matches": [
+			"context-menu"
+		],
+		"category": "links-and-status",
+		"vendor": false,
+	},
+	{
+		"type": "copy",
+		"matches": [
+			"copy"
+		],
+		"category": "drag-and-drop",
+		"vendor": false,
+	},
+	{
+		"type": "crosshair",
+		"matches": [
+			"crosshair"
+		],
+		"category": "selection",
+		"vendor": false,
+	},
+	{
+		"type": "default",
+		"matches": [
+			"default"
+		],
+		"category": "general",
+		"vendor": false,
+	},
+	{
+		"type": "e-resize",
+		"matches": [
+			"e-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "ew-resize",
+		"matches": [
+			"ew-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "grab",
+		"matches": [
+			"grab"
+		],
+		"category": "grab",
+		"vendor": false,
+	},
+	{
+		"type": "grabbing",
+		"matches": [
+			"grabbing"
+		],
+		"category": "grab",
+		"vendor": false,
+	},
+	{
+		"type": "help",
+		"matches": [
+			"help"
+		],
+		"category": "links-and-status",
+		"vendor": false,
+	},
+	{
+		"type": "inherit",
+		"matches": [
+			"inherit"
+		],
+		"category": "general",
+		"vendor": false,
+	},
+	{
+		"type": "initial",
+		"matches": [
+			"initial"
+		],
+		"category": "general",
+		"vendor": false,
+	},
+	{
+		"type": "move",
+		"matches": [
+			"move"
+		],
+		"category": "drag-and-drop",
+		"vendor": false,
+	},
+	{
+		"type": "n-resize",
+		"matches": [
+			"n-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "ne-resize",
+		"matches": [
+			"ne-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "nesw-resize",
+		"matches": [
+			"nesw-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "no-drop",
+		"matches": [
+			"no-drop"
+		],
+		"category": "drag-and-drop",
+		"vendor": false,
+	},
+	{
+		"type": "none",
+		"matches": [
+			"none"
+		],
+		"category": "general",
+		"vendor": false,
+	},
+	{
+		"type": "not-allowed",
+		"matches": [
+			"not-allowed"
+		],
+		"category": "drag-and-drop",
+		"vendor": false,
+	},
+	{
+		"type": "ns-resize",
+		"matches": [
+			"ns-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "nw-resize",
+		"matches": [
+			"nw-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "nwse-resize",
+		"matches": [
+			"nwse-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "pointer",
+		"matches": [
+			"pointer"
+		],
+		"category": "links-and-status",
+		"vendor": false,
+	},
+	{
+		"type": "progress",
+		"matches": [
+			"progress"
+		],
+		"category": "links-and-status",
+		"vendor": false,
+	},
+	{
+		"type": "row-resize",
+		"matches": [
+			"row-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "s-resize",
+		"matches": [
+			"s-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "se-resize",
+		"matches": [
+			"se-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "sw-resize",
+		"matches": [
+			"sw-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "text",
+		"matches": [
+			"text"
+		],
+		"category": "selection",
+		"vendor": false,
+	},
+	{
+		"type": "unset",
+		"matches": [
+			"unset"
+		],
+		"category": "general",
+		"vendor": false,
+	},
+	{
+		"type": "url(\"https://plus.google.com/_/favicon?domain=google.com\"), auto",
+		"matches": [
+			"url(\"https://plus.google.com/_/favicon?domain=google.com\"), auto"
+		],
+		"category": "custom",
+		"vendor": false,
+	},
+	{
+		"type": "vertical-text",
+		"matches": [
+			"vertical-text"
+		],
+		"category": "selection",
+		"vendor": false,
+	},
+	{
+		"type": "w-resize",
+		"matches": [
+			"w-resize"
+		],
+		"category": "resize-and-scrolling",
+		"vendor": false,
+	},
+	{
+		"type": "wait",
+		"matches": [
+			"wait"
+		],
+		"category": "links-and-status",
+		"vendor": false,
+	},
+	{
+		"type": "zoom-in",
+		"matches": [
+			"zoom-in"
+		],
+		"category": "zoom",
+		"vendor": false,
+	},
+	{
+		"type": "zoom-out",
+		"matches": [
+			"zoom-out"
+		],
+		"category": "zoom",
+		"vendor": false,
+	},
+];
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import CURSORS from './data/cursors';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const cursors = document.getElementById('cursors');
+	while (cursors.firstChild) {
+		cursors.removeChild(cursors.firstChild);
+	}
+
+	const list = document.createDocumentFragment();
+
+	CURSORS
+		.forEach(function (cursor) {
+			const type = cursor.type;
+			const matches = cursor.matches;
+			const item = document.createElement('li');
+			item.classList.add('cursor-demo');
+			item.dataset.cursor = type;
+			item.style.cursor = type;
+			item.textContent = type;
+			list.appendChild(item);
+
+			const style = item.style.cursor;
+			const isContained = matches.indexOf(style) >= 0;
+			item.classList.add(isContained ? 'style-applied' : 'style-failed');
+		});
+
+	cursors.appendChild(list);
+});
