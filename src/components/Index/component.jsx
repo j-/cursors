cd82a28da2f7e500516379d7c93b38c5e54c2a77
j- <j@skeoh.com>
2016-03-14 00:08:47 +1100

    Add React Bootstrap

--- src/components/Index/component.jsx
+++ src/components/Index/component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { PageHeader } from 'react-bootstrap';
 import { Link } from 'react-router';
 
 export default class Index extends React.Component {
 	render () {
 		return (
 			<div className="container">
+				<PageHeader>Cursors</PageHeader>
 				<Link to="/">List cursors</Link><br />
 				<Link to="/test">Test cursors</Link>
 				{ this.props.children }
